1f19001e3bc77d66cccb22d9d5e26acc9580c621
widyantarif <55181778+widyantarif@users.noreply.github.com>
2019-10-22 12:26:27 +0700

    Delete dbConnect.php

--- tugas2-web-lentera-studios/dbConnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$hostServerName = "localhost";
-	$hostUserName = "root";
-	$hostPassWord = "";
-	$hostDataBase = "trofhee";
-
-	$connect = mysqli_connect($hostServerName, $hostUserName, $hostPassWord, $hostDataBase);
-?>
