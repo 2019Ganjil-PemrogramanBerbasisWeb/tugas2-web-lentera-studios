56e815baace831fe7be78d73c18a6fa826fa666e
Fikri Haykal <fhaykal24@gmail.com>
2019-10-30 15:15:22 +0700

    Create dbConnect.php

---
+++ function/dbConnect.php
@@ -0,0 +1,8 @@
+<?php
+	$hostServerName = "localhost";
+	$hostUserName = "root";
+	$hostPassWord = "";
+	$hostDataBase = "trofhee";
+
+	$connect = mysqli_connect($hostServerName, $hostUserName, $hostPassWord, $hostDataBase);
+?>
