839a37840d76fdcd13800c0ef394374d31ff3a28
Fikri Haykal <fhaykal24@gmail.com>
2019-10-30 16:18:47 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,5 +3,11 @@
   session_start();
   session_destroy();
 
+  if(isset($_COOKIE['login']))
+    {
+    $time = time();
+    setcookie("login", $time - 3600);
+    }
+
   header("location:../tugas2-web-lentera-studios/");
 ?>
