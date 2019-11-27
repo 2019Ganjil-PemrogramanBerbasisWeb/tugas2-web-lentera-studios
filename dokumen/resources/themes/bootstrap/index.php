c0b26628894679416378d9675eaf01b3564ceb51
Fikri Haykal <fhaykal24@gmail.com>
2019-11-27 15:13:16 +0700

    Update index.php

--- dokumen/resources/themes/bootstrap/index.php
+++ dokumen/resources/themes/bootstrap/index.php
@@ -98,7 +98,7 @@
             <ul id="directory-listing" class="nav nav-pills nav-stacked">
                 <?php
                     $filelist = array();
-                    if ($handle = opendir("..%2F")) {
+                    if ($handle = opendir("F:\Web Development\XAMPP\htdocs\tugas2-web-mini\")) {
                         while ($entry = readdir($handle)) {
                                 $filelist[] = $entry;
                                 echo $entry."test";
