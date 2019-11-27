d778ea29d4e14632c34dba46af8945f0f1027cb1
Fikri Haykal <fhaykal24@gmail.com>
2019-11-27 14:47:48 +0700

    Update index.php

--- dokumen/resources/themes/bootstrap/index.php
+++ dokumen/resources/themes/bootstrap/index.php
@@ -100,9 +100,8 @@
                     $filelist = array();
                     if ($handle = opendir(".")) {
                         while ($entry = readdir($handle)) {
-                            if (strpos($entry, "te") === 0) {
                                 $filelist[] = $entry;
-                            }
+    
                         }
                         closedir($handle);
                     }
