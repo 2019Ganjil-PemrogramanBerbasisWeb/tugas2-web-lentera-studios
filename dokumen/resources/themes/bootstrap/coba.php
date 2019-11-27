9cce81f085ca9c539676d58e7c64181180fd42e0
Fikri Haykal <fhaykal24@gmail.com>
2019-11-27 14:35:51 +0700

    Update coba.php

--- dokumen/resources/themes/bootstrap/coba.php
+++ dokumen/resources/themes/bootstrap/coba.php
@@ -1,5 +1,5 @@
 <?php
-  if ($handle = opendir('http://10.161.20.20/tugas2-web-mini/')) {
+  if ($handle = opendir('../tugas2-web-mini/')) {
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
             echo "<a href='download.php?file=".$entry."'>".$entry."</a>\n";
